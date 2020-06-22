63c044d7b2e2e41f27eed2e579ce1d49aa525fd2
Christophe Avonture <christophe.avonture@bosa.fgov.be>
2020-06-22 14:50:47 +0200

    feat: Add regions :eight_spoked_asterisk:

---
+++ 020-working-with-code/080-regions/files/regions.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * BOSA Xxxxxxx project
+ * -- Type your a quick description of your project --
+ * php version 7.4
+ *
+ * @package   Xxxxxxx
+ * @author    FirstName LastName <firstname.lastname@bosa.fgov.be>
+ * @copyright 2018-2020 (c) SPF BOSA IOD AS - PHP Dev Team
+ * @license   Proprietary
+ *
+ * @link https://code.bosa.fgov.be/dg-r_o/happ/happ-limesurvey
+ * @wiki https://code.bosa.fgov.be/dg-r_o/happ/happ-limesurvey/-/wikis/home
+ *
+ * Latest version: https://code.bosa.fgov.be/##URL##
+ * Documentation:  https://code.bosa.fgov.be/##URL##/-/wikis/home
+ */
+
+function addRows(array $data): array
+{
+    $result = [];
+
+    //region 1. Add row only once for the same surveyId/sessionId
+    foreach ($data as $participant) {
+        // A few dozen lines of code...
+        // A few dozen lines of code...
+        // A few dozen lines of code...
+    }
+
+    //endregion
+
+    //region 2. Add new participants
+    try {
+        // A few dozen lines of code...
+        // A few dozen lines of code...
+        // A few dozen lines of code...
+    } catch (\Exception $e) {
+        // Handle the exception
+    }
+
+    //endregion
+
+    //region 3. Prepare resulting array
+    // A few dozen lines of code...
+    // A few dozen lines of code...
+    // A few dozen lines of code...
+    // A few dozen lines of code...
+    //endregion
+
+    return $result;
+}
---
+++ 020-working-with-code/080-regions/readme.md
@@ -0,0 +1,14 @@
+# regions
+
+`Regions` are a very smart features to make code editing more readable.
+
+Consider the following function:
+
+```php
+<!-- concat-md::include "./files/regions.php" -->
+```
+
+Your function is, logically, divided into three parts; the first to do checks, the second to do the job and the third to prepare the return data.
+
+Using `regions` inside your function make the code's logic really easy to understand.
+
