d5c95d26edf15fa5277f2ad594a4eee538f12442
Christophe Avonture <christophe.avonture@bosa.fgov.be>
2022-01-17 09:56:08 +0100

    docs: updated doc

--- concat-md.sh
+++ concat-md.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-OUTPUT_FILE="readme.md"
-
-clear
-
-SCRIPT_DIR="$(cd "$(dirname "${0}")" &>/dev/null && pwd)"
-
-# Make sure the script is running from his folder so relative paths are correct
-# shellcheck disable=SC2164
-pushd "$SCRIPT_DIR" &>/dev/null
-
-"$WRITING_DOC/concat-md/concat-md.sh" --output "$OUTPUT_FILE" --mode pdf --verbose
-
-# shellcheck disable=SC2164
-popd &>/dev/null
+docker run --rm -v "${PWD}"/:/data bosa/writing-doc concat -o readme.md
